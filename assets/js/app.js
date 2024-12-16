bf41234f731a3d06153e7c82a27d480e06177944
david <davidike444@gmail.com>
2024-12-16 03:56:23 -0600

    updated code to follow guidelines

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,47 @@
 'use strict';
 
-// Helper functions for DOM selection
-function select(selector, scope = document) {
-  return scope.querySelector(selector);
-}
+import { select, selectAll, listen } from "./utils.js";
 
-const loginForm = select('form');
-const usernameInput = select('input[placeholder="email or phone"]');
-const passwordInput = select('input[placeholder="password"]');
+const API_URL = 'https://randomuser.me/api/?results=3'; 
+const randomContainer = select('.random-container'); 
+const nameElements = selectAll('.random-container h3'); 
+const jobElements = selectAll('.random-container .job'); 
+const imageElements = selectAll('.random-container img'); 
 
-// Set default credentials in localStorage
-localStorage.setItem('username', 'davidike444@gmail.com');
-localStorage.setItem('password', '123456');
+listen('load', window, () => {
+    getRandomUsers();
+});
 
+async function getRandomUsers() {
+  try {
+    const response = await fetch(API_URL);
 
-function validateLogin(event) {
-  event.preventDefault(); 
+    if (!response.ok) {
+      throw new Error(`${response.statusText} (${response.status})`);
+    }
 
-  // Get user input
-  const enteredUsername = usernameInput.value.trim();
-  const enteredPassword = passwordInput.value.trim();
+    const data = await response.json();
+    const users = data.results;
 
-  const storedUsername = localStorage.getItem('username');
-  const storedPassword = localStorage.getItem('password');
-
-  if (enteredUsername === storedUsername && enteredPassword === storedPassword) {
-    window.location.href = 'home.html'; 
-  } else {
-   
-    const errorMsg = select('.wrong-password');
-    errorMsg.classList.remove('hidden'); 
+    populateRandomUsers(users);
+  } catch (error) {
+    console.error(error.message);
   }
 }
 
+function populateRandomUsers(users) {
+    users.forEach((user, index) => {
+        if (nameElements[index]) {
+            nameElements[index].textContent = `${user.name.first} ${user.name.last}`;
+        }
 
-loginForm.addEventListener('submit', validateLogin);
+        if (jobElements[index]) {
+            jobElements[index].textContent = `${user.location.city}, ${user.location.state}`;
+        }
+
+        if (imageElements[index]) {
+            imageElements[index].src = user.picture.medium;
+            imageElements[index].classList.add('profile-photo');
+        }
+    });
+}
